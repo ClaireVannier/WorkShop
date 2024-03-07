ba63148ea517ef3966211606d592911e4635c784
Claire Vannier <clairevannierpro@gmail.com>
2024-03-07 17:15:25 +0100

    fonctionnal reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { CardComponent } from "./card/card.component";
 import { CatalogComponent } from "./catalog/catalog.component";
 import { SearchComponent } from "./search/search.component";
 import { StudentDetailComponent } from './student-details/student-details.component';
+import { WorkshopFormulaireComponent } from "./sign-up-form/sign-up-form.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, CardComponent, CatalogComponent, SearchComponent, StudentDetailComponent]
+    imports: [RouterOutlet, CardComponent, CatalogComponent, SearchComponent, StudentDetailComponent, WorkshopFormulaireComponent]
 })
 export class AppComponent {
   title = 'WorkShop';
